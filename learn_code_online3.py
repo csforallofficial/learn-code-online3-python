728da3feca2419fa66e6e79edc84bf2129663ddd
Sunyul Hossen <60459287+xenon-suny@users.noreply.github.com>
2020-02-02 11:19:20 +0530

    finish README
    solution file

---
+++ learn_code_online3.py
@@ -0,0 +1,6 @@
+#learn code online3 solution
+big_cup=500
+small_cup=300
+required_water=big_cup-small_cup
+print("we first pour water fully into the big cup of 500ml, then we take the small cup of 300 ml and pour water from the big cup into the small cup. So the amount of water left in the big cup is exactly (big cup-small cup)= ",required_water)
+
